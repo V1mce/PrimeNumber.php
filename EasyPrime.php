674505de34725861aedca59e7b6f5abdf35d4049
V1mce <94705570+V1mce@users.noreply.github.com>
2021-11-19 18:58:15 +0100

    Update and rename Primo.php to EasyPrime.php

--- EasyPrime.php
+++ EasyPrime.php
@@ -1,14 +1,13 @@
 <?php
 $numero = $_POST['numero'];
 $count = 2;
-$primo=0;
-while($count<=$numero/2 && $primo==0){
+while($count<=$numero){
     if ($numero % $count==0) {
-    $primo=1;}
+    break;}
     else{
     $count++;}
 }
-if ($primo==0){
+if ($count=$numero){
     echo $numero.' Es primo';}      
 else {
     echo $numero." No es primo";}        
